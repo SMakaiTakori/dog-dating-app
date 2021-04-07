eb2c995313b37a7287744c05b6575281cd141c57
SMakaiTakori <makait87@icloud.com>
2021-04-07 15:03:24 -0400

    pulled/updated after redux store was added

--- dog-dating-app-client/src/components/Login.js
+++ dog-dating-app-client/src/components/Login.js
@@ -1,36 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Login extends Component {
-    
-    state={
-        name:'',
-        email:'', 
-        password:''
-        
-    }
-    
-    
-    
-    
-    render() {
-        return (
-            <div>
-                <form className="login">
-                    <br />
-                   
-                    <br />
-                    <label>Email:</label><br />
-                    <input type='email' value={this.state.email} onChange={event => this.handleChange(event)} name='email' />
-                    <br />
-                    <br />
-                    <label>Password:</label><br />
-                    <input type='password' value={this.state.name} onChange={event => this.handleChange(event)} name='password' />
-                    <br />
+  state = {
+    name: "",
+    email: "",
+    password: "",
+  };
 
-                </form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <form className="login">
+          <br />
+
+          <br />
+          <label>Email:</label>
+          <br />
+          <input
+            type="email"
+            value={this.state.email}
+            onChange={(event) => this.handleChange(event)}
+            name="email"
+          />
+          <br />
+          <br />
+          <label>Password:</label>
+          <br />
+          <input
+            type="password"
+            value={this.state.name}
+            onChange={(event) => this.handleChange(event)}
+            name="password"
+          />
+          <br />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Login;
